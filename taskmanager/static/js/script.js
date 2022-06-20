48fd0754ddc5a7b74ab6be9c89fd40597cf1798c
AndrosDe <andreas_beier1981@yahoo.de>
2022-06-20 12:15:16 +0000

    adding navbar and footer

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initallisation
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
+  
